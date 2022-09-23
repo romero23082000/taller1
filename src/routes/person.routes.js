5af7627017def0ce5c54e68394f129c91c5ea938
romero23082000 <andreshito435@gmail.com>
2022-09-22 21:40:59 -0500

    Logica metodos delete, update y obtener por id

--- src/routes/person.routes.js
+++ src/routes/person.routes.js
@@ -17,9 +17,29 @@ person_routes.get('/person', (req, res)=>{
     .then((data)=>res.json(data))
     .catch((err)=>res.json({message: err}))
 })
-person_routes.get('/', (req, res)=>{})
-person_routes.put('/', (req, res)=>{})
-person_routes.delete('/', (req, res)=>{})
+person_routes.get('/person/:id', (req, res)=>{
+    const {id} = req.params
+    user_schema.
+    findById(id)
+    .then((data)=>res.json(data))
+    .catch((err)=>res.json({message: err}))
+    
+})
+person_routes.put('/person/:id', (req, res)=>{
+    const {id} = req.params
+    const{user_name, lastname, age, email, proffession}=req.body
+    user_schema.
+    updateOne({_id:id},{$set:{user_name, lastname, age, email, proffession}})
+    .then((data)=>res.json(data))
+    .catch((err)=>res.json({message: err}))
+})
+person_routes.delete('/person/:id', (req, res)=>{
+    const {id} = req.params
+    user_schema.
+    remove({_id:id})
+    .then((data)=>res.json(data))
+    .catch((err)=>res.json({message: err}))
+})
 
 
 module.exports = person_routes;
